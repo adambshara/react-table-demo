2cc5dcda8d864805401855dc0da15e65bfe84d69
ahmad <ab.bshara1995@gmail.com>
2022-12-16 14:40:22 -0500

    Getting info from table

---
+++ src/components/columns.jsx
@@ -0,0 +1,34 @@
+export const Columns = [
+  {
+    Header: "Id",
+    accessor: "id",
+  },
+  {
+    Header: "First Name",
+    accessor: "first_name",
+  },
+  {
+    Header: "Last Name",
+    accessor: "last_name",
+  },
+  {
+    Header: "E-mail",
+    accessor: "email",
+  },
+  {
+    Header: "DOB",
+    accessor: "date_of_birth",
+  },
+  {
+    Header: "Age",
+    accessor: "age",
+  },
+  {
+    Header: "Country",
+    accessor: "country",
+  },
+  {
+    Header: "Phone Number",
+    accessor: "phone",
+  },
+];
